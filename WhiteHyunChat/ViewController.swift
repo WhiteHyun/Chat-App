7e88c69a9ce9d497de71ab1a37ef6ff378fa6d0d
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-05-07 14:30:56 +0900

    Initial Commit

---
+++ WhiteHyunChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteHyunChat
+//
+//  Created by 홍승현 on 2022/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
