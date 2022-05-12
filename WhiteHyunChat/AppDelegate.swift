f0ca8a7352260bf335bf2304a563ad0430f0065a
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-05-12 22:29:13 +0900

    :fire: Remove test code

--- WhiteHyunChat/AppDelegate.swift
+++ WhiteHyunChat/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     FirebaseApp.configure()
     let _ = Firestore.firestore()
-    try? Auth.auth().signOut()
+
     IQKeyboardManager.shared.enable = true
     IQKeyboardManager.shared.enableAutoToolbar = false
     IQKeyboardManager.shared.shouldResignOnTouchOutside = true
